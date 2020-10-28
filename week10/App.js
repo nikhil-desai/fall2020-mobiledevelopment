a44ae2842ff4847fd62a7b6256783353b86ffa73
Nikhil Desai <nikhildesigning@gmail.com>
2020-10-27 22:00:48 -0400

    Development Assignment 4

---
+++ week10/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Name: Nikhil Desai</Text>
+      <Text>Favorite Food: Thai Red Curry!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
